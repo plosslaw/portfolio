781fef7d159eb1d539b3019d8b5ca0cb23be3ada
plosslaw <spacequartz@gmail.com>
2020-02-29 16:51:20 +0800

    added fade in out animation prevent highlighting of footer bar prevent clicking on email icon while popup appears

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,10 +31,12 @@
 
 })(jQuery);
 
+var popup = document.querySelector("#email")
+var emailIcon = document.querySelector("#email-icon")
 
 function showEmail() {
 	// var popup = document.getElementById("email");
-	var popup = document.querySelector("#email");
+	// var popup = document.querySelector("#email");
 	var dummy = document.createElement("textarea");
 	dummy.style.display = 'hidden';
 	document.body.appendChild(dummy);
@@ -42,6 +44,26 @@ function showEmail() {
 	dummy.select();
 	document.execCommand("copy");
 	document.body.removeChild(dummy);
+	toggleOn().then(setTimeout(hideEmail,3100));
+
 	
-	popup.classList.toggle("show");
   }
+
+//   function sleep(ms) {
+// 	return new Promise(resolve => setTimeout(resolve, ms));
+//   }
+function toggleOn(){
+	// var popup = document.querySelector("#email");
+	return new Promise(
+		resolve => {
+			emailIcon.classList.add("blockClick");
+			popup.classList.add("show");
+			// setTimeout(hideEmail,3000);
+		}
+	);
+}
+
+function hideEmail(){
+	popup.classList.remove("show");
+	emailIcon.classList.remove("blockClick");
+}
